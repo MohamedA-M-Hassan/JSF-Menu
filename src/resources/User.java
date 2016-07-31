4b5f90eaf35ca95bce063587eab1adf0c8263ab2
MohamedA-M-Hassan <anehmr0@gmail.com>
2016-07-31 12:32:04 -0300

    fix

---
+++ src/resources/User.java
@@ -0,0 +1,36 @@
+package resources;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class User implements Serializable{
+	private String name;
+	private String password;
+
+	public User() {
+		super();
+		name = "Default Name";
+		password = "Default Password";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
