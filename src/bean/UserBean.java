e3d44a445e024f8ee231376611a749e4d3e83ca4
MohamedA-M-Hassan <anehmr0@gmail.com>
2016-08-01 11:33:14 -0300

    User Bean

--- src/bean/UserBean.java
+++ src/bean/UserBean.java
@@ -1,9 +1,10 @@
-/*package bean;
+package bean;
 
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.persistence.Column;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -14,6 +15,49 @@ import resources.User;
 @ManagedBean
 @SessionScoped
 public class UserBean implements Serializable {
+
+	private String userName;
+	private String name;
+	private int phone;
+	private String password;
+	private String passwordCheck;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPasswordCheck() {
+		return passwordCheck;
+	}
+	public void setPasswordCheck(String passwordCheck) {
+		this.passwordCheck = passwordCheck;
+	}
+	
+	
+	
+	
+	/* HABAL
 	public void insertUser(String name, String pass)
 	{
 		
@@ -39,6 +83,5 @@ public class UserBean implements Serializable {
 		session.getTransaction().commit();
 		session.close();
 	} 
-
-}
 */
+}
